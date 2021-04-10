af8169d09891484120f9da9538a60de3c994355f
Lea Ann Bradford <leaann@notch8.com>
2021-04-09 19:21:22 -0700

    story-7 add new exercise form, route, controller method

--- exercise_app/app/controllers/exercises_controller.rb
+++ exercise_app/app/controllers/exercises_controller.rb
@@ -6,4 +6,8 @@ class ExercisesController < ApplicationController
   def show
     @exercise = Exercise.find(params[:id])
   end
+
+  def new
+    @exercise = Exercise.new
+  end
 end
--- exercise_app/config/routes.rb
+++ exercise_app/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'exercises#index'
   get 'exercises' => 'exercises#index', as: 'exercises'
+  get 'exercises/new' => 'exercises#new', as: 'new_exercise'
   get 'exercises/:id' => 'exercises#show', as: 'exercise'
 end
