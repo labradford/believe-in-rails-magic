2d7b4c449e5bde90e92aecddb8ab6e7b61a03fee
Lea Ann Bradford <leaann@notch8.com>
2021-04-09 19:10:47 -0700

    story 4 - add excercises controller, index method, index view

---
+++ exercise_app/app/controllers/exercises_controller.rb
@@ -0,0 +1,5 @@
+class ExercisesController < ApplicationController
+  def index
+    @exercises = Exercise.all
+  end
+end
--- exercise_app/config/routes.rb
+++ exercise_app/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'exercises#index'
+  get 'exercises' => 'exercises#index', as: 'exercises'
 end
